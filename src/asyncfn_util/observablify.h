47e0afeff55d3143445d31546ec1a5f736a7bd93
terukazu inoue <terukazu.inoue@codianz.com>
2021-03-06 22:44:29 +0900

    リファクタリング

--- src/asyncfn_util/observablify.h
+++ src/asyncfn_util/observablify.h
@@ -27,13 +27,10 @@ public:
 #if __cplusplus >= 201703L
   auto rx(typename SUPER::OP_F_ARGS args) const {
     return rxcpp::observable<>::create<typename SUPER::CB_ARGS>([=](rxcpp::subscriber<typename SUPER::CB_ARGS> s){
-      typename SUPER::STATICIFY sfn([=](auto&& ...results){
+      (*this)(args, [=](auto&& ...results){
         s.on_next(typename SUPER::CB_ARGS{results...});
         s.on_completed();
       });
-      auto refcon_callback = std::tuple<typename SUPER::REFCON_TYPE, typename SUPER::CB_F_TYPE>(reinterpret_cast<typename SUPER::REFCON_TYPE>(sfn.refcon()), sfn.callback());
-      auto params = std::tuple_cat(args, refcon_callback);
-      std::apply(this->target(), params);
     });
   }
 #endif
